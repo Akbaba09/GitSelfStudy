b8108a077efa98b42bed56ce0e48c9431601d6ae
Coskun Akbaba <coskunakbaba0955@gmail.com>
2023-02-27 13:40:13 +0100

    User Registration and Self Practice files added

---
+++ src/test/java/SelfPractice.java
@@ -0,0 +1,8 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class SelfPractice {
+    public static void main(String[] args) {
+        System.out.println("Do practice ");
+
+    }
+}
---
+++ src/test/java/UserLogin/UserRegistration.java
@@ -0,0 +1,7 @@
+package UserLogin;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Registration");
+    }
+}
